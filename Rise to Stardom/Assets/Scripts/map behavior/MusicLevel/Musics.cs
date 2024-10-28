d031a3b30bb12b275658666a4f984a9ef1ae9f15
AlexUwUr <gastoncolpari2015@gmail.com>
2024-10-27 22:30:25 -0300

    changes

---
+++ Rise to Stardom/Assets/Scripts/map behavior/MusicLevel/Musics.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musics : MonoBehaviour
+{
+    public AudioClip background;
+    public AudioClip fight;
+
+}
